854af589fbad246c1432a2ea2c92212ca3e81f0b
Jordan Toor <gammasts@gmail.com>
2011-12-13 16:47:39 -0800

    Python script that will read EC2 tags and write them to sourceable file as well as an ini file

---
+++ ec2_tags_to_file.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+# Gets instance metadata and ec2 tags and writes them out to a sourceable bash file and ini file for reading elsewhere
+import urllib
+from boto.ec2.connection import EC2Connection
+
+conn = EC2Connection("AWS_ACCESS_KEY", "AWS_SECRET_KEY")
+reservations = conn.get_all_instances()
+instances = [i for r in reservations for i in r.instances]
+
+instance_id = urllib.urlopen('http://169.254.169.254/latest/meta-data/instance-id').read()
+hostname = urllib.urlopen('http://169.254.169.254/latest/meta-data/local-hostname').read()
+
+print("%s" % instance_id)
+
+f_bash = open('/tmp/instance-metadata', 'w')
+f_config = open('/tmp/metadata-config.ini', 'w')
+
+f_config.write("[EC2_TAGS]\n")
+
+for i in instances:
+    if i.id == instance_id:
+        tags = i.tags
+        for k, v in tags.items():
+            f_bash.write("export EC2_%s=%s\n" % (k.upper(), v))
+            f_config.write("%s: %s\n" % (k, v))
+
+f_bash.close()
+f_config.close()
